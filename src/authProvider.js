512291ac50d1251bec72881274dd18da1db159d4
Kevin Zellweger <kevin.zellweger@mimacom.com>
2021-07-12 17:53:58 +0200

    JWT Authentication

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,53 +1,44 @@
-import {
-    AUTH_GET_PERMISSIONS,
-    AUTH_LOGIN,
-    AUTH_LOGOUT,
-    AUTH_ERROR,
-    AUTH_CHECK,
-} from 'react-admin'; // eslint-disable-line import/no-unresolved
+import inMemoryJWT from './inMemoryJwt';
 
-// Authenticatd by default
-export default (type, params) => {
-    if (type === AUTH_LOGIN) {
-        const { username, password } = params;
-        if (username === 'login' && password === 'password') {
-            localStorage.removeItem('not_authenticated');
-            localStorage.removeItem('role');
-            return Promise.resolve();
-        }
-        if (username === 'user' && password === 'password') {
-            localStorage.setItem('role', 'user');
-            localStorage.removeItem('not_authenticated');
-            return Promise.resolve();
-        }
-        if (username === 'admin' && password === 'password') {
-            localStorage.setItem('role', 'admin');
-            localStorage.removeItem('not_authenticated');
-            return Promise.resolve();
-        }
-        localStorage.setItem('not_authenticated', true);
-        return Promise.reject();
-    }
-    if (type === AUTH_LOGOUT) {
-        localStorage.setItem('not_authenticated', true);
-        localStorage.removeItem('role');
+const authProvider = {
+    login: ({ username, password }) => {
+        let headers = new Headers();
+        headers.append('Authorization', 'Basic ' + Buffer.from(username + ':' + password).toString('base64'));
+        headers.append('Content-Type', 'application/json');
+        const request = new Request('http://localhost:5000/api/users/login', {
+            method: 'GET',
+            headers: headers
+        });
+        return fetch(request)
+            .then((response) => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                let token = response.headers.get('Authorization')
+                inMemoryJWT.setToken(token);
+            })
+    },
+    logout: () => {
+        inMemoryJWT.ereaseToken();
         return Promise.resolve();
-    }
-    if (type === AUTH_ERROR) {
-        const { status } = params;
-        return status === 401 || status === 403
-            ? Promise.reject()
-            : Promise.resolve();
-    }
-    if (type === AUTH_CHECK) {
-        return localStorage.getItem('not_authenticated')
-            ? Promise.reject()
-            : Promise.resolve();
-    }
-    if (type === AUTH_GET_PERMISSIONS) {
-        const role = localStorage.getItem('role');
-        return Promise.resolve(role);
-    }
+    },
 
-    return Promise.reject('Unknown method');
+    checkAuth: () => {
+        return inMemoryJWT.getToken() ? Promise.resolve() : Promise.reject();
+    },
+
+    checkError: (error) => {
+        const status = error.status;
+        if (status === 401 || status === 403) {
+            inMemoryJWT.ereaseToken();
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    },
+
+    getPermissions: () => {
+        return inMemoryJWT.getToken() ? Promise.resolve() : Promise.reject();
+    },
 };
+
+export default authProvider;
--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,8 +1,19 @@
 import {fetchUtils} from 'react-admin';
 import {stringify} from  'querystring';
-
+import inMemoryJWT from './inMemoryJwt';
 const apiUrl = 'http://localhost:5000/api';
-const httpClient = fetchUtils.fetchJson;
+const httpClient = (url) => {
+    const options = {
+        headers: new Headers({ Accept: 'application/json' }),
+    };
+    const token = inMemoryJWT.getToken();
+    if (token) {
+        options.headers.set('Authorization', `Bearer ${token}`);
+    }
+
+    return fetchUtils.fetchJson(url, options);
+};
+
 
 export default {
     getList: (resource, headers, params) => {
