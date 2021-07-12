fac6f547804995455fa6a1da95117fcf43290e8f
Kevin Zellweger <kevin.zellweger@students.fhnw.ch>
2021-07-12 08:06:26 +0200

    add dummy auth provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import dataProvider from './dataProvider';
+import authProvider from "./authProvider";
+import MyLoginPage from "./MyLoginPage";
 import {UserList} from "./components/users";
 import {ClientsList} from "./components/clients";
 
 
 function App() {
   return (
-      <Admin dataProvider={dataProvider}>
+      <Admin dataProvider={dataProvider} authProvider={authProvider}>
         <Resource name='users' list={UserList}/>
         <Resource name='clients/all' list={ClientsList}/>
       </Admin>
