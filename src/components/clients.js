db87bd9bae4f18d04548bdbe927b4243d0ece37d
Kevin Zellweger <kevin.zellweger@mimacom.com>
2021-07-13 12:43:25 +0200

    custom EditForm

--- src/components/clients.js
+++ src/components/clients.js
@@ -1,12 +1,23 @@
 import * as React from 'react';
-import {List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import {Datagrid, List, ReferenceField, TextField, Edit, SimpleForm, TextInput, PasswordInput} from 'react-admin';
 
 export const ClientsList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="name"/>
             <TextField source="id"/>
-            <ReferenceField source="userId" reference="users"><TextField source="userName" /></ReferenceField>
+            <ReferenceField source="userId" reference="users"><TextField source="userName"/></ReferenceField>
         </Datagrid>
     </List>
+);
+
+export const UserEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput source="id"/>
+            <TextInput source="userName"/>
+            <PasswordInput source="password"/>
+            <TextInput source="role"/>
+        </SimpleForm>
+    </Edit>
 );
